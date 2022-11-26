a06dd07171d1ab08b18ac3ee8fd285e589d259b4
charakaw <mininuwan@gmail.com>
2022-11-26 16:12:09 +0530

    commit on 26Nov2022 1611

--- src/test/java/io/mega/AssignmentOne.java
+++ src/test/java/io/mega/AssignmentOne.java
@@ -54,11 +54,11 @@ public class AssignmentOne {
 
     @Test
     void assignmentOneTest() throws IOException {
-        wait_MiliSec(4000);
+        wait_MiliSec(5000);
 
         WebElement loginBtn = driver.findElement(By.xpath("(//button[@class='mega-button top-login-button'])[1]"));
         loginBtn.click();
-        wait_MiliSec(3000);
+        wait_MiliSec(5000);
 
         WebElement inputLoginName = driver.findElement(By.xpath("//input[@name='login-name2']"));
         WebElement inputLoginPassword = driver.findElement(By.xpath("//input[@name='login-password2']"));
@@ -72,17 +72,17 @@ public class AssignmentOne {
         Actions actions = new Actions(driver);
         WebElement rightClickBody = driver.findElement(By.xpath("//div[@class='megaList-content']"));
         actions.contextClick(rightClickBody).perform();
-        wait_MiliSec(2000);
+        wait_MiliSec(3000);
 
         WebElement optNewFile = driver.findElement(By.xpath("//span[text()='New text file']"));
         if(optNewFile.isDisplayed()){
             optNewFile.click();
-            wait_MiliSec(2000);
+            wait_MiliSec(3000);
 
             WebElement inputNewFileName = driver.findElement(By.xpath("//input[@name='dialog-new-file']"));
             String fileName = RandomStringUtils.randomAlphabetic(3);
             inputNewFileName.sendKeys(fileName);
-            wait_MiliSec(2000);
+            wait_MiliSec(3000);
 
             WebElement btnCreate = driver.findElement(By.xpath("//button[@class='mega-button positive fm-dialog-new-file-button create-file']"));
             btnCreate.click();
@@ -90,47 +90,50 @@ public class AssignmentOne {
 
             WebElement txtBody = driver.findElement(By.xpath("//pre[@class=' CodeMirror-line ']/span"));
             String txtBodyContent = RandomStringUtils.randomAlphabetic(10);
+            txtBodyContent = "megatesting";
+
+            new Actions(driver).sendKeys(txtBodyContent).perform();
 
             //txtBody.sendKeys(Keys.ENTER);
             //JavascriptExecutor js = (JavascriptExecutor)driver;
             //js.executeScript("arguments[0].innerHTML='Berlín'", txtBody);
 
-            //WebElement btnSave = driver.findElement(By.xpath("//button[@class='mega-button positive gradient save-btn']"));
-            //btnSave.click();
+            WebElement btnSave = driver.findElement(By.xpath("//button[@class='mega-button positive gradient save-btn']"));
+            btnSave.click();
             wait_MiliSec(5000);
 
             WebElement btnCloseTextArea = driver.findElement(By.xpath("(//button[@class='close-btn']) [2]"));
             btnCloseTextArea.click();
-            wait_MiliSec(4000);
+            wait_MiliSec(5000);
 
             String xPathFileName = "(//span[text()='" + fileName + ".txt']) [1]";
             //span[text()='OHZ.txt']
             //span[@class='data-block-bg ']
             WebElement savedFile = driver.findElement(By.xpath(xPathFileName));
             actions.contextClick(savedFile).perform();
-            wait_MiliSec(2000);
+            wait_MiliSec(3000);
 
 
             WebElement moveToBin = driver.findElement(By.xpath("//a[@class='dropdown-item remove-item']"));
             if(moveToBin.isDisplayed()){
                 moveToBin.click();
-                wait_MiliSec(2000);
+                wait_MiliSec(3000);
 
                 WebElement btnMoveToBinYes = driver.findElement(By.xpath("(//button[@class='mega-button positive confirm']) [1]"));
                 btnMoveToBinYes.click();
-                wait_MiliSec(2000);
+                wait_MiliSec(3000);
             }
 
             //move to Rubbish bin
             WebElement btnBinMenu = driver.findElement(By.xpath("(//button[normalize-space()='Rubbish bin'])[1]"));
             btnBinMenu.click();
-            wait_MiliSec(2000);
+            wait_MiliSec(3000);
 
             //span[text()='sss.txt']
             String xRubbishFileName = "//span[text()='" + fileName + ".txt']";
             WebElement deletedFile = driver.findElement(By.xpath(xRubbishFileName));
             actions.contextClick(deletedFile).perform();
-            wait_MiliSec(2000);
+            wait_MiliSec(3000);
 
             WebElement optRestore = driver.findElement(By.xpath("//a[@class='dropdown-item revert-item']"));
             if(optRestore.isDisplayed()) {
